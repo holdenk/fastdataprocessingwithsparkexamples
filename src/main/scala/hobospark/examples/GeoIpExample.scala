363c5e113f19c83d361826d8a94c40503d052aea
Holden Karau <holden@pigscanfly.ca>
2013-06-03 22:00:26 -0700

    Update

--- src/main/scala/hobospark/examples/GeoIpExample.scala
+++ src/main/scala/hobospark/examples/GeoIpExample.scala
@@ -1,5 +1,7 @@
 package hobospark.examples
 
+import scala.math
+
 import spark.SparkContext
 import spark.SparkContext._
 import spark.SparkFiles;
@@ -7,6 +9,7 @@ import spark.util.Vector
 
 import au.com.bytecode.opencsv.CSVReader
 
+import java.util.Random
 import java.io.StringReader
 import java.io.File
 
@@ -42,11 +45,9 @@ object GeoIpExample {
       }
     })
     val geoFile = sc.addFile(maxMindPath)
-
     // getLocation gives back an option so we use flatMap to only output if its a some type
-    val ipGeo = IpGeo(dbFile = maxMindPath)
-    val ipCountries = parsedInput.flatMap(pair => {
-     ipGeo.getLocation(pair._1).map(c => (pair._1, c.countryCode))
+    val ipCountries = parsedInput.flatMapWith(_ => IpGeo(dbFile = SparkFiles.get(maxMindPath) ))((pair, ipGeo) => {
+     ipGeo.getLocation(pair._1).map(c => (pair._1, c.countryCode)).toSeq
      })
     ipCountries.cache()
     val countries = ipCountries.values.distinct().collect()
@@ -57,6 +58,15 @@ object GeoIpExample {
 	case (countryData, originalData) => DataPoint(new Vector(countryData++originalData.slice(1,originalData.size-2)) , originalData(originalData.size-1))
       }
     })
-    println("Data points :"+dataPoints.collect())
+    countriesSignal.cache()
+    dataPoints.cache()
+    val rand = new Random(53)
+    var w = Vector(dataPoints.first.x.length, _ => rand.nextDouble)
+    for (i <- 1 to iterations) {
+      val gradient = dataPoints.map(p =>
+	(1 / (1 + math.exp(-p.y*(w dot p.x))) - 1) * p.y * p.x).reduce(_ + _)
+      w -= gradient
+    }
+    println("Final w: "+w)
   }
 }

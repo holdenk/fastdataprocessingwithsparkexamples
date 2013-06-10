28245e6299051d5f4f4bc9dcccc6ebfd28249e5b
Holden Karau <holden@pigscanfly.ca>
2013-06-09 21:25:44 -0700

    Add test

---
+++ src/test/scala/hobospark/examples/MoreTestableLoadCsvExampleSuite.scala
@@ -0,0 +1,23 @@
+package hobospark.examples
+
+import spark._
+import spark.SparkContext._
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+
+class MoreTestableLoadCsvExampleSuite extends FunSuite with ShouldMatchers {
+  test("summ data on input") {
+    val sc = new SparkContext("local", "Load CSV Example")
+    val counter = sc.accumulator(0)
+    val input = sc.parallelize(List("1,2","1,3"))
+    val result = MoreTestableLoadCsvExample.handleInput(counter, input)
+    result.collect() should equal (Array[Int](3,4))
+  }
+  test("should parse a csv line with numbers") {
+    MoreTestableLoadCsvExample.parseLine("1,2") should equal (Array[Double](1.0,2.0))
+    MoreTestableLoadCsvExample.parseLine("100,-1,1,2,2.5") should equal (Array[Double](100,-1,1.0,2.0,2.5))
+  }
+  test("should error if there is a non-number") {
+    evaluating { MoreTestableLoadCsvExample.parseLine("pandas")  } should produce [NumberFormatException]
+  }
+}
